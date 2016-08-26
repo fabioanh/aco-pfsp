16a16360a4d480c1bafef295698945670dde7dfc
NAVARRETE Fabio (ext) <u510290@bpost.be>
2016-08-26 18:16:22 +0100

    Main addition of methods to compute the pheromone update. Switch hash tables to matrices

--- src/main/java/be/ac/intelligence/swarm/Ant.java
+++ src/main/java/be/ac/intelligence/swarm/Ant.java
@@ -6,36 +6,70 @@ import org.apache.commons.lang3.SerializationUtils;
 
 public class Ant {
 	private PermutationFlowShopProblem problem;
-	private Integer[][] pheromone;
-	private Integer[][] heuristicInformation;
+	private Double[][] pheromone;
+	private Double[][] initialPheromone;
+	private Double[][] heuristicInformation;
 	private Double beta;
+	private Double pheromoneDecayCoeficient;
+
+	public Ant(final PermutationFlowShopProblem problem, final Double[][] pheromone,
+			final Double[][] heuristicInformation, final Double beta, final Double pheromoneDecayCoeficient) {
+		this.problem = problem;
+		this.pheromone = PfspUtils.deepCopy(pheromone);
+		this.initialPheromone = PfspUtils.deepCopy(pheromone);
+		this.heuristicInformation = PfspUtils.deepCopy(heuristicInformation);
+		this.beta = beta;
+		this.pheromoneDecayCoeficient = pheromoneDecayCoeficient;
+	}
 
 	/**
 	 * Copy Constructor
 	 * 
 	 * @param ant
 	 */
-	public Ant(Ant ant) {
+	public Ant(final Ant ant) {
 		problem = SerializationUtils.clone(ant.getProblem());
-		this.pheromone = ant.pheromone;
+		this.pheromone = ant.getPheromone();
 		this.heuristicInformation = ant.getHeuristicInformation();
 		this.beta = ant.getBeta();
+		this.pheromone = ant.getPheromone();
+		this.initialPheromone = ant.getPheromone();
+		this.pheromoneDecayCoeficient = ant.getPheromoneDecayCoeficient();
 	}
 
 	/**
-	 * Random proportional rule computation
+	 * Random proportional rule probability computation
 	 * 
 	 * @param set
 	 * @param sets
 	 * @return
 	 */
-	private Double randomProportionalProbability(Integer set, ArrayList<Integer> sets) {
-		Double numerator = pheromone.get(set) * Math.pow(heuristicInformation.get(set), beta);
+	private Double randomProportionalProbability(int i, int j, ArrayList<Integer> candidateList) {
+		Double numerator = pheromone[i][j] * Math.pow(heuristicInformation[i][j], beta);
 		Double denominator = 0.0;
-		for (Integer s : sets) {
-			denominator += pheromone.get(s) * Math.pow(heuristicInformation.get(s), beta);
+		for (Integer l : candidateList) {
+			denominator += pheromone[i][l] * Math.pow(heuristicInformation[i][l], beta);
 		}
 		return numerator / denominator;
+	}
+
+	/**
+	 * Method to locally update the pheromone values
+	 */
+	private void updatePheromone() {
+		int solSize = problem.getSolution().size();
+		if (solSize > 1) {
+			this.updatePheromoneSingle(problem.getSolution().get(solSize - 2), problem.getSolution().get(solSize - 1));
+		}
+	}
+
+	/**
+	 * Method to update a single value of the pheromone based on its indices for
+	 * the pheromone matrix
+	 */
+	private void updatePheromoneSingle(int i, int j) {
+		this.pheromone[i][j] = (1.0 - pheromoneDecayCoeficient) * pheromone[i][j]
+				+ (pheromoneDecayCoeficient * initialPheromone[i][j]);
 	}
 
 	public PermutationFlowShopProblem getProblem() {
@@ -46,19 +80,19 @@ public class Ant {
 		this.problem = problem;
 	}
 
-	public Integer[][] getPheromone() {
+	public Double[][] getPheromone() {
 		return pheromone;
 	}
 
-	public void setPheromone(Integer[][] pheromone) {
+	public void setPheromone(Double[][] pheromone) {
 		this.pheromone = pheromone;
 	}
 
-	public Integer[][] getHeuristicInformation() {
+	public Double[][] getHeuristicInformation() {
 		return heuristicInformation;
 	}
 
-	public void setHeuristicInformation(Integer[][] heuristicInformation) {
+	public void setHeuristicInformation(Double[][] heuristicInformation) {
 		this.heuristicInformation = heuristicInformation;
 	}
 
@@ -69,4 +103,13 @@ public class Ant {
 	public void setBeta(Double beta) {
 		this.beta = beta;
 	}
+
+	public Double getPheromoneDecayCoeficient() {
+		return pheromoneDecayCoeficient;
+	}
+
+	public void setPheromoneDecayCoeficient(Double pheromoneDecayCoeficient) {
+		this.pheromoneDecayCoeficient = pheromoneDecayCoeficient;
+	}
+
 }
--- src/main/java/be/ac/intelligence/swarm/AntSolver.java
+++ src/main/java/be/ac/intelligence/swarm/AntSolver.java
@@ -1,18 +1,21 @@
 package be.ac.intelligence.swarm;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class AntSolver {
 
 	private PermutationFlowShopProblem problem;
-	private HashMap<Integer, Double> pheromone;
-	private HashMap<Integer, Double> heuristicInformation;
+	private Double[][] pheromone;
+	private Double[][] heuristicInformation;
 	private ArrayList<Ant> ants;
 	private Ant bestAnt;
+	// Pheromone evaporation rate
+	private Double rho;
 
 	private AntSolver(String instance) {
 		problem = new PermutationFlowShopProblem(instance);
+		initHeuristicInformation();
+		this.pheromone = new Double[problem.getNumJobs()][problem.getNumJobs()];
 	}
 
 	public static class AntSolverBuilder {
@@ -32,8 +35,56 @@ public class AntSolver {
 		// TODO Auto-generated method stub
 	}
 
-	public void updatePhereomone(Ant currentBestAnt) {
+	/**
+	 * Updates pheromone in a global way for all node values
+	 * 
+	 * @param minimumMakespan
+	 */
+	public void updatePhereomone(Integer minimumMakespan) {
+		// TODO: Optionally add evaporate pheromone using concepts of min-max
+		for (int i = 0; i < this.problem.getNumJobs(); i++) {
+			for (int j = 0; j < this.problem.getNumJobs(); j++) {
+				updatePheromone(i, j, minimumMakespan);
+			}
+		}
+	}
 
+	/**
+	 * Updates the pheromone nodes contained in the solution sequence
+	 * 
+	 * @param solution
+	 * @param minimumMakespan
+	 */
+	public void updatePheromone(ArrayList<Integer> solution, Integer minimumMakespan) {
+		for (int i = 0; i < solution.size() - 1; i++) {
+			updatePheromone(solution.get(i), solution.get(i + 1), minimumMakespan);
+		}
+	}
+
+	/**
+	 * Extracted method to allow pheromone updates only to specific nodes
+	 * depending on conditions given by the algorithm
+	 * 
+	 * @param i
+	 * @param j
+	 * @param minimumMakespan
+	 */
+	public void updatePheromone(Integer i, Integer j, Integer minimumMakespan) {
+		this.pheromone[i][j] = (1 - rho) * this.pheromone[i][j] + (rho / minimumMakespan);
+	}
+
+	/**
+	 * Computes the distance between each of the jobs using the makespan
+	 * computation between two jobs (i, j) as distance value stored in a two
+	 * dimensional array for easy access
+	 */
+	private void initHeuristicInformation() {
+		this.heuristicInformation = new Double[problem.getNumJobs()][problem.getNumJobs()];
+		for (int i = 0; i < problem.getNumJobs(); i++) {
+			for (int j = 0; j < problem.getNumJobs(); j++) {
+				this.heuristicInformation[i][j] = 1.0 / this.problem.computeMakespan(i, j);
+			}
+		}
 	}
 
 }
